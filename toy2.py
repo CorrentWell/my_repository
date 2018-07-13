6ca9c480190e19d7cc0a632c523b17404a891cf0
CorrentWell <imai.h.ad@m.titech.ac.jp>
2018-07-13 20:27:48 +0900

    完成！

--- toy2.py
+++ toy2.py
@@ -5,10 +5,19 @@ Created on Fri Jul 13 19:33:22 2018
 @author: yg2018
 """
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
+np.random.seed(1)
 n = 40
-omega = np.random.randn(1, n)
+omega = np.random.randn(1)
 noise = 0.8 * np.random.randn(n, 1)
 x = np.random.randn(n, 2)
-y = 2 * (omega * x[:, 1] + x[:, 2] + noise > 0) - 1
+tf = (omega * x[:, 0] + x[:, 1]).reshape(n,1) + noise > 0
+y = 2 * tf - 1
+
+P = np.array([X for X, Y in zip (x, y) if Y > 0])
+N = np.array([X for X, Y in zip (x, y) if Y < 0])
+
+plt.plot(P[:,0], P[:,1], "r.", label="Positive")
+plt.plot(N[:,0], N[:,1], "b.", label="Negative")
+plt.legend()
