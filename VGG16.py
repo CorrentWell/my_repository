05218c2de36f13ff6674a3db097c70e6ded27e99
CorrentWell <imai.h.ad@m.titech.ac.jp>
2018-07-27 17:30:39 +0900

    単純にフォルダ分けだけしてある。本当はsklearnでデータ割り振りしたい。それとkerasの関数について読み込むようにしなければならない。

--- VGG16.py
+++ VGG16.py
@@ -7,19 +7,28 @@ from keras.layers import Input, Activation, Dropout, Flatten, Dense
 from keras.preprocessing import image
 from keras import optimizers
 
-classes = ['chino', 'cocoa', 'chiya', 'rize', 'syaro']
+classes = ["BG", "HCC"]
 nb_classes = len(classes)
 img_width, img_height = 150, 150
 
 result_dir = 'results'
 
 # このディレクトリにテストしたい画像を格納しておく
-test_data_dir = 'dataset/test'
+test_data_dir = 'for_VGG16\validation'
 
 def model_load():
     # VGG16, FC層は不要なので include_top=False
     input_tensor = Input(shape=(img_width, img_height, 3))
     vgg16 = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor)
+    """
+    include_topはVGG16のトップにある1000クラス分類するフル結合層（FC）を含むか含まないかを指定する。
+    今回は画像分類を行いたいためFCを含んだ状態で使う。FCを捨ててVGG16を特徴抽出器として使うことでいろいろ面白いことができるがまた今度取り上げたい。
+    weightsはVGG16の重みの種類を指定する。VGG16は単にモデル構造であるため必ずしもImageNetを使って学習しなければいけないわけではない。
+    しかし、現状ではImageNetで学習した重みしか提供されていない。Noneにするとランダム重みになる。自分で集めた画像で学習する猛者はこちらか？
+    input_tensorは自分でモデルに画像を入力したいときに使うが今回は未使用。あとでVGG16のFine-tuningをする際に使う。
+    input_shapeは入力画像の形状を指定する。include_top=Trueにして画像分類器として使う場合は (224, 224, 3) で固定なのでNoneでOK。
+    何か中途半端な解像度だけどこれがImageNetの標準サイズのようだ。
+    """
 
     # FC層の作成
     top_model = Sequential()
